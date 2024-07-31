218586e9804136f1ac7184d48873adfa63497b39
Patrick <patricktsaikl@gmail.com>
2024-08-01 07:10:59 +0800

    feat/examples of test case

---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module.
+    add is a function in calc module
+    """
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(2,3)
+
+        self.assertEqual(res,5)
+
+    def test_subtract_numbers(self):
+        """Test subtract numbers"""
+        res = calc.subtract(5,1)
+
+        self.assertEqual(res,4)
