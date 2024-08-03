207604b7e0fe7af232834b14b085aafee3c1e595
Patrick <patricktsaikl@gmail.com>
2024-08-03 09:15:54 +0800

    style/fix warning from lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module.
     add is a function in calc module
@@ -13,12 +14,12 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(2,3)
+        res = calc.add(2, 3)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
 
     def test_subtract_numbers(self):
         """Test subtract numbers"""
-        res = calc.subtract(5,1)
+        res = calc.subtract(5, 1)
 
-        self.assertEqual(res,4)
+        self.assertEqual(res, 4)
