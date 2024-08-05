a268751bfa0c2a26a57c4f8fc6e046148ea29284
Patrick <patricktsaikl@gmail.com>
2024-08-06 07:18:27 +0800

    feat/implement command wait_for_db

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ docker-compose run --rm app sh -c "django-admin startproject app ."
 generate django app:
 docker-compose run --rm app sh -c "python manage.py startapp core"
 
+execute the command method:
+docker-compose run --rm app sh -c "python manage.py wait_for_db"
+
 To enable precommit:
 
 1. download pre-commit
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,12 @@
 """
 Django command to wait for the db to be available.
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+# error django throws while db is not ready
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -8,4 +14,15 @@ class Command(BaseCommand):
     """Django command to wait for db"""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError) as e:
+                self.stdout.write(
+                    f'db unavailable due to {type(e)}, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('db available'))
